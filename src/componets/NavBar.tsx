0a2dc8064cbec4e13d99a42b060b21cdd2a0ccac
zhaohao fan <fanzhaohao889@gmail.com>
2024-04-27 23:23:53 -0400

    Bulid ColorModeSwitch

--- src/componets/NavBar.tsx
+++ src/componets/NavBar.tsx
@@ -1,12 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/bootstrap-logo-shadow.png";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 // NavBar component
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent={"space-between"} padding={"20px"}>
       <Image src={logo} boxSize={"70px"} />
-      <Text>NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
