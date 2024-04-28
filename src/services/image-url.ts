4dd717d3884076180b1eb1a5e8edf70a0a72cf0b
zhaohao fan <fanzhaohao889@gmail.com>
2024-04-28 04:34:32 -0400

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,8 @@
+import noImage from "../assets/noImagePlatform.jpeg";
+
 // This function takes an image URL and returns a new URL with the same image cropped to 600x400 pixels.
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
